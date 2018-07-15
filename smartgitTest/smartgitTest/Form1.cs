a17ea3ddc141df2e3d450b51d8cc94783372ad3d
QuinllunLi <857033209@qq.com>
2018-07-15 16:13:47 +0800

    新建的工程

---
+++ smartgitTest/smartgitTest/Form1.cs
@@ -0,0 +1,31 @@
+﻿using smartgitTest.class1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace smartgitTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "hello world!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = test1.changestr();
+        }
+    }
+}
---
+++ smartgitTest/smartgitTest/class1/test1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace smartgitTest.class1
+{
+    class test1
+    {
+        public static string changestr()
+        {
+            return "aaaaaaaaaaaaaaa";
+        }
+    }
+}
